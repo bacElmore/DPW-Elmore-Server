170e5183bf3a144e5b5d6efdaabf5a4b6b464926
bacElmore <bac.tran@uselmore.com>
2021-06-22 21:38:19 -0700

    first com

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+//Set View Engine for EJS
+const app = express();
+
+const mysql = require('mysql');
+var path = require('path');
+require('dotenv/config');
+const readline = require('readline');
+const Joi = require('joi');
+const ejs = require('ejs');
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+const flash = require('express-flash');
+const session = require('express-session');
+const methodOverride = require('method-override');
+const request = require('request');
+//FOR SENDING EMAIL
+const nodeMailer = require('nodemailer');
+const hbs = require('nodemailer-express-handlebars');
+
+
+//Root page
+app.get('/', (req,res) => {
+    res.json('Test: "Test"');
+});
+
+const serverPort = process.env.PORT;
+app.listen(serverPort, () => console.log(`Listening on Port ${serverPort}`));
